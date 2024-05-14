a279b610af1de1f47221ec0a021890146d94df02
Casey <74725509+q3sssi@users.noreply.github.com>
2024-05-15 02:24:50 +0300

    add

--- scripts.js
+++ scripts.js
@@ -2,4 +2,12 @@ var b = 0;
 function plus(){
     const a = new Date();
     document.getElementById("mainblock").innerHTML = b++;
+    
+    current = document.getElementById("menu").style.display;
+    if(current == none){
+        current = display;
+    }
+    else{
+        current = none;
+    }
 }
