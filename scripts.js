ac7082946576ca0754d5627eb99c2b2323db08a8
Casey <74725509+q3sssi@users.noreply.github.com>
2024-05-17 00:23:38 +0300

    add

--- scripts.js
+++ scripts.js
@@ -22,15 +22,9 @@ tg.expand();
 
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id)) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     elmnt.onmousedown = dragMouseDown;
-  }
-
+    
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
