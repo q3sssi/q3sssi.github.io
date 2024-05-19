361acfa8e0de94284107abe42edef90cd3239487
Casey <74725509+q3sssi@users.noreply.github.com>
2024-05-19 05:13:06 +0300

    add

--- scripts.js
+++ scripts.js
@@ -14,10 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
             accept: ".ui-widget-content",
             drop: function( event, ui ) {
                 $( this ).addClass( "ui-state-highlight" ).find( "p" );
-                var $list = $( "ul", $(".droparea") ).length ?
-                $( "ul", $(".droparea") ) :
-                $( "<ul class='gallery ui-helper-reset'/>" ).appendTo( $(".droparea") );
-              
                 snapToMiddle(ui.draggable,$(this));
             }
         });
