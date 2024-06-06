e43c82c880c13a784233b2a34e5753f61e3bd430
Casey <74725509+q3sssi@users.noreply.github.com>
2024-06-06 04:00:43 +0300

    add

--- scripts.js
+++ scripts.js
@@ -31,10 +31,8 @@ let usercard = document.getElementById("usercard"); //получаем блок 
 let profName = document.createElement('p'); //создаем параграф
 profName.innerText = `${tg.initDataUnsafe.user.first_name}
 ${tg.initDataUnsafe.user.last_name}
-${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
+${tg.initDataUnsafe.user.username} 
+${tg.initDataUnsafe.user.id}
+${tg.initDataUnsafe.user.language_code}`;
 //выдем имя, "фамилию", через тире username и код языка
 usercard.appendChild(profName); //добавляем 
-
-let userid = document.createElement('p'); //создаем еще параграф 
-userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
-usercard.appendChild(userid); //добавляем
